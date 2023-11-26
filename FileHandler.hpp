c9c7dd809f44a7b0ef0eecc1c4d44a5a051b6068
dtanigaw <dtanigaw@student.42.fr>
2023-11-26 17:53:14 +0100

    first

---
+++ FileHandler.hpp
@@ -0,0 +1,19 @@
+#ifndef _FILEHANDLER_HPP
+# define _FILEHANDLER_HPP
+
+#include <fstream>
+
+class	_c_FileHandler
+{
+	public:
+		_FileHandler();
+		~_FileHandler();
+
+		
+
+	private:
+		_filename;
+		_key;
+};
+
+#endif
